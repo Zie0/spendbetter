d4b48ff71979d63d20c3ac62dce478b927c6a608
Zie0 <ethan.locke@zoho.com>
2017-03-22 21:02:09 -0600

    added clean date. updated gitignore

--- budget.py
+++ budget.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+import datetime as dt
 
 def load_csv(csvfile):
     # Read the csv
@@ -10,6 +11,14 @@ def load_csv(csvfile):
 
 def clean_memo(df):
     df['Memo'] = df['Memo'].apply(lambda x: x.split(";")[1].strip() if str.isdigit(x.split(";")[1].strip()) else str(0))
+    return df
+
+def clean_date(df):
+    df['Date'] = df['Date'].apply(lambda x: dt.datetime.strptime(x, "%m/%d/%Y"))
+    return df
+
+def clean_df(df):
+    df=clean_memo(clean_date(df))
     return df
 
 if __name__=="__main__":
