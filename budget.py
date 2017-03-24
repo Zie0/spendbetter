b67c2dc70cb665c97eba9bdd085ee7af82cedf0d
Zie0 <ethan.locke@zoho.com>
2017-03-23 21:19:58 -0600

    fixed methods and __init__

--- budget.py
+++ budget.py
@@ -6,30 +6,30 @@ import datetime as dt
 
 class Statement:
     def __init__(self,csvfile,acct='c'):
-        self.statement = load_csv(csvfile,acct)
+        self.statement = self.load_csv(csvfile,acct)
 
-    def load_csv(csvfile,acct='c'):
+    def load_csv(self,csvfile,acct='c'):
         # Read the csv
         data = pd.read_csv(csvfile,parse_dates=['Date'],index_col=0,low_memory=False)
         if acct == 'c':
             try:
-                data = clean_credit_memo(data)
+                data = self.clean_credit_memo(data)
             except Exception as E:
                 print("This file may not be a (credit) csv.", E)
         elif acct == 'd':
             try:
-                data = clean_debit_memo(data)
+                data = self.clean_debit_memo(data)
             except Exception as E:
                 print("This file may not be a (debit) csv.", E)
         else:
             print("Account Type parameter must be a string:\n\t'c' (credit)\n\t'd' (debit)\nIf ommitted default Account Type is (credit)")
         return data
 
-    def clean_credit_memo(df):
+    def clean_credit_memo(self,df):
         df['Memo'] = df['Memo'].apply(lambda x: x.split(";")[1].strip() if str.isdigit(x.split(";")[1].strip()) else str(0))
         return df
 
-    def clean_debit_memo(df):
+    def clean_debit_memo(self,df):
         df['Memo'] = df['Memo'].apply(lambda x: x.split(".")[2].strip())
         return df
 
@@ -39,8 +39,6 @@ class Statement:
             return self.statement[parsed]
         except Exception as E:
             print("Could not parse date or month out of range: {0}\nPlease Enter a month in 'YYYY-mm' format".format(E))
-
-
 
 if __name__=="__main__":
     #output the number of rows
